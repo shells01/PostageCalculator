1ffb76b87fa508a09b4eb4bbeac32d3c1c59af89
Shelly <shells01@users.noreply.github.com>
2017-02-17 16:35:40 -0800

    Add files via upload

---
+++ ChallengePostageCalculatorHelperMethods/ChallengePostageCalculatorHelperMethods/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePostageCalculatorHelperMethods.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .newStyle1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h4><span class="newStyle1">&nbsp;Postal Calculator</span></h4>
+        <br />
+&nbsp;Width:&nbsp;
+        <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+        <br />
+&nbsp;Height:&nbsp;
+        <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+        <br />
+&nbsp;Length:&nbsp;
+        <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="postageGroup" OnCheckedChanged="groundRadioButton_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="postageGroup" OnCheckedChanged="airRadioButton_CheckedChanged" Text="Air" />
+        <br />
+        <asp:RadioButton ID="nextDayRadioButton" runat="server" AutoPostBack="True" GroupName="postageGroup" OnCheckedChanged="nextDayRadioButton_CheckedChanged" Text="Next Day" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostageCalculatorHelperMethods/ChallengePostageCalculatorHelperMethods/Default.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostageCalculatorHelperMethods
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            entryChanged();
+        }
+
+        private void entryChanged()
+        {
+            // Check if required entries are filled
+            if (!entrySelected()) return;
+            
+            // Get the volume
+            int volume = 0;
+            if (!getVolume(out volume)) return;
+
+            // multiplier
+            double postageMultiplier = getPostageMultiplier();
+
+            // Calculate the cost
+            double cost = volume * postageMultiplier;
+            // Display the price
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", cost);
+        }
+
+        private bool entrySelected()
+        {
+            // check if required entries are filled and radio buttons are selected
+            if (!airRadioButton.Checked
+                && !groundRadioButton.Checked
+                && !nextDayRadioButton.Checked)
+                return false;
+
+            if (widthTextBox.Text.Trim().Length == 0
+                || heightTextBox.Text.Trim().Length == 0)
+                return false;
+
+            return true;
+        }
+
+        private bool getVolume(out int volume)
+        {
+            // get the volume
+            // initial values
+            volume = 0;
+            int width = 0;
+            int height = 0;
+            int length = 0;
+
+            // set length = 1 if no entry for length
+            if (!int.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!int.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!int.TryParse(lengthTextBox.Text.Trim(), out length)) length = 1;
+
+            // calculate the volume
+            volume = width * height * length;
+
+            return true;
+        }
+
+        
+        private double getPostageMultiplier()
+        {
+            if (groundRadioButton.Checked) return 0.15;
+            else if (airRadioButton.Checked) return 0.25;
+            else if (nextDayRadioButton.Checked) return 0.45;
+            else return 0;
+        }
+    }
+}
